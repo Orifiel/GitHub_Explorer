2fe57bda74c58cd5c94d2fa96d229a226342c2d2
Cleiton Gomes <cleiton.gomes@abecom.local>
2020-12-16 09:27:35 -0300

    teste de branch

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore Repositorios no GitHub</Title>;
-};
+  return (
+    <>
+      <img src={logoImg} alt="Github Explores" />
+      <Title>Explore Repositorios no Github</Title>
 
+      <Form>
+        <input placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/49954230?s=460&u=32fe74955f68b81ba984adc1b20419cc49b047d1&v=4"
+            alt="Cleiton"
+          />
+          <div>
+            <strong>CardAnimatedCthulhu</strong>
+            <p>Card animado, utilizando apenas CSS e Javascript</p>
+          </div>
+        </a>
+      </Repositories>
+    </>
+  );
+};
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,7 +1,66 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
-  text-align: left;
-  font-size: 2rem;
+  font-size: 48px;
   color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    padding: 24px 24px;
+    color: #3a3a3a;
+  }
+
+  button {
+    background: #04d361;
+    border: 0;
+    border-radius: 0 5px 5px 0;
+    color: white;
+    width: 210px;
+    height: 70px;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.form`
+  a {
+    max-width: 700px;
+    height: 110px;
+    display: flex;
+    position: relative;
+  }
+
+  img {
+    position: absolute;
+    width: 80px;
+    height: 80px;
+    top: 16px;
+    left: 16px;
+  }
+
+  div {
+    position: absolute;
+  }
 `;
