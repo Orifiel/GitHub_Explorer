85c7b9e7b24ea488a30b14e048427c37b4fda658
Orifiel <cleitonmattos87@gmail.com>
2021-06-02 19:29:12 -0300

    Correção da pagina Repository

--- src/pages/Repository/styles.ts
+++ src/pages/Repository/styles.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
+interface FormProps {
+  hasError: boolean;
+}
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 export const RepositoryInfo = styled.section`
   margin-top: 80px;
   header {
@@ -14,11 +24,11 @@ export const RepositoryInfo = styled.section`
       margin-left: 24px;
       strong {
         font-size: 36px;
-        color: ${props => props.theme.colors.titleSecundary};
+        color: #3d3d4d;
       }
       p {
         font-size: 18px;
-        color: ${props => props.theme.colors.text};
+        color: #737380;
         margin-top: 4px;
       }
     }
@@ -34,12 +44,12 @@ export const RepositoryInfo = styled.section`
       strong {
         display: block;
         font-size: 36px;
-        color: ${props => props.theme.colors.titleSecundary};
+        color: #3d3d4d;
       }
       span {
         display: block;
         margin-top: 4px;
-        color: ${props => props.theme.colors.text};
+        color: #6c6c80;
       }
     }
   }
@@ -47,8 +57,9 @@ export const RepositoryInfo = styled.section`
 
 export const Issues = styled.div`
   margin-top: 80px;
+  max-width: 700px;
   a {
-    background: ${props => props.theme.colors.shape};
+    background: #fff;
     border-radius: 5px;
     width: 100%;
     padding: 24px;
@@ -56,28 +67,34 @@ export const Issues = styled.div`
     text-decoration: none;
     display: flex;
     align-items: center;
-    & + a {
-      margin-top: 16px;
-    }
     transition: transform 0.2s;
+    & + a {
+      margin-top: 10px;
+    }
     &:hover {
       transform: translateX(10px);
+    }
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
     }
     div {
       margin: 0 16px;
       flex: 1;
       strong {
         font-size: 20px;
-        color: ${props => props.theme.colors.titleSecundary};
+        color: #3d3d4d;
       }
       p {
         font-size: 18px;
-        color: ${props => props.theme.colors.textSecundary};
+        color: #a8a8b3;
         margin-top: 4px;
       }
     }
     svg {
       margin-left: auto;
+      color: #cbcbd6;
     }
   }
 `;
